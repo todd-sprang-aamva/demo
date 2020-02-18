6b8446f7a3fe5d0b3c7880a3686b750a5fc90081
Todd Sprang <tsprang@aamva.org>
2020-02-18 16:34:17 -0500

    missing comma

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace demo
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello Azure!");
+                    await context.Response.WriteAsync("Hello, Azure!");
                 });
             });
         }
